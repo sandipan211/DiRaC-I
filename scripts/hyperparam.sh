dce5df2184cc8fcbd13da5531a2e0880b076ac28
Sandipan Sarma <sandipan.sarma@iitg.ac.in>
2024-06-03 18:18:18 +0530

    Update hyperparam.sh

--- scripts/hyperparam.sh
+++ scripts/hyperparam.sh
@@ -2,7 +2,7 @@
 cd /home/gdata/sandipan/BTP2021
 
 # hyperparameter sensitivity study
-# python3 dirac_i.py -d 'CUB' -sn 1 -cq 3 -es 25 -c -l 'ward' -lr 0.01 -m 'latest_trained_model' -t 100 -o 'new_seed_final' -r 0.8 -n 150 -s 5 -w 0.5
+python3 dirac_i.py -d 'CUB' -sn 1 -cq 3 -es 25 -c -l 'ward' -lr 0.01 -m 'latest_trained_model' -t 100 -o 'new_seed_final' -r 0.8 -n 150 -s 5 -w 0.5
 # python3 dirac_i.py -d 'CUB' -sn 1 -cq 4 -es 25 -c -l 'ward' -lr 0.01 -m 'latest_trained_model' -t 100 -o 'new_seed_final' -r 0.8 -n 150 -s 5 -w 0.5
 # python3 dirac_i.py -d 'SUN' -sn 1 -cq 5 -es 25 -c -l 'ward' -lr 0.001 -m 'latest_trained_model' -t 580 -o 'new_seed_final' -r 0.8 -n 645 -s 5 -w 0.5
 # python3 dirac_i.py -d 'SUN' -sn 1 -cq 7 -es 25 -c -l 'ward' -lr 0.001 -m 'latest_trained_model' -t 580 -o 'new_seed_final' -r 0.8 -n 645 -s 5 -w 0.5
