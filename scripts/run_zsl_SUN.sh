da4b90395fd0cf1b95fed9dfdeff35002c275c6f
Sandipan Sarma <sandipan.sarma@iitg.ac.in>
2025-02-04 13:14:00 +0530

    Update run_zsl_SUN.sh

--- scripts/run_zsl_SUN.sh
+++ scripts/run_zsl_SUN.sh
@@ -61,7 +61,6 @@
 
 
 # # For TFVAEGAN
-# # carefully check all new params with what keerti ran
 
 # cd /home/gdata/sandipan/BTP2021/new_zsl_models/TFVAEGAN/tfvaegan-master
 # CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python3 train_images.py --sn 1 --al_lr 0.001 --gammaD 1 --gammaG 1 --gzsl --manualSeed 4115 --encoded_noise --preprocessing --cuda --image_embedding res101 --class_embedding att --nepoch 400 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 --dataset SUN --batch_size 64 --nz 102 --latent_size 102 --attSize 102 --resSize 2048 --lr 0.001 --classifier_lr 0.0005 --syn_num 400 --nclass_all 717 --recons_weight 0.01 --a1 0.1 --a2 0.01 --feedback_loop 2 --feed_lr 0.0001 --al_seed 'original'
@@ -75,10 +74,6 @@
 # python3 train_images.py --sn 1 --al_lr 0.001 --cq 8 --gammaD 1 --gammaG 1 --manualSeed 4115 --encoded_noise --preprocessing --image_embedding res101 --class_embedding att --nepoch 400 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 --dataset SUN --batch_size 64 --nz 102 --latent_size 102 --attSize 102 --resSize 2048 --lr 0.001 --classifier_lr 0.0005 --syn_num 400 --nclass_all 717 --recons_weight 0.01 --a1 0.1 --a2 0.01 --feedback_loop 2 --feed_lr 0.0001 --al_seed 'new_seed_final'
 
 
-# For SAGAN
-# cd /home/gdata/sandipan/BTP2021/new_zsl_models/SAGAN
-
-# python3 sagan.py --dataset SUN --sn 1 --al_lr 0.001 --al_seed 'original' --d_lr 0.005 --g_lr 0.005 --hl 1024 --t 0.05 --paper_gamma 0.00001 
 
 
 # For CNZSL
@@ -247,7 +242,6 @@ python3 MSDN_sun.py --dataset SUN --sn 2 --al_lr 0.001 --fname 'MSDN' --gzsl --w
 
 
 # # For TFVAEGAN
-# # carefully check all new params with what keerti ran
 
 # cd /home/gdata/sandipan/BTP2021/new_zsl_models/TFVAEGAN/tfvaegan-master
 # CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python3 train_images.py --sn 3 --al_lr 0.001 --gammaD 1 --gammaG 1 --gzsl --manualSeed 4115 --encoded_noise --preprocessing --cuda --image_embedding res101 --class_embedding att --nepoch 400 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 --dataset SUN --batch_size 64 --nz 102 --latent_size 102 --attSize 102 --resSize 2048 --lr 0.001 --classifier_lr 0.0005 --syn_num 400 --nclass_all 717 --recons_weight 0.01 --a1 0.1 --a2 0.01 --feedback_loop 2 --feed_lr 0.0001 --al_seed 'original'
