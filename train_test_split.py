8a083d0d3eff3a2e860d97edee25fd6301ff844d
Sandipan Sarma <sandipan.sarma@iitg.ac.in>
2025-02-04 13:04:55 +0530

    Update train_test_split.py

--- train_test_split.py
+++ train_test_split.py
@@ -1,23 +1,10 @@
-# from random import sample
-# import shutil
-# import os
-# for dir in os.listdir('Animals_with_Attributes2/JPEGImages/'):
-#   path = os.path.join('Animals_with_Attributes2/JPEGImages/',dir)
-#   temp = os.listdir(path)
-#   lst = sample(temp, int(0.1 * len(temp)))
-#   for f in temp:
-#     if f in lst:
-#       shutil.copy(os.path.join(path,f), 'test_dir/')
-#     else:
-#       shutil.copy(os.path.join(path,f), 'train_dir/')
 
-# see dataset_sun.ipynb for SUN dataset
 
 
 from random import sample
 import shutil
 import os
-for dir in os.listdir('CUB/Data/'):
+for dir in os.listdir('CUB/Data/'): # replace with SUN for SUN dataset
   path = os.path.join('CUB/Data/', dir)
   temp = os.listdir(path)
   lst = sample(temp, int(0.1 * len(temp)))
